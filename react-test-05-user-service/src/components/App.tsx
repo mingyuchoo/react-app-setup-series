d47491f89c7f164d55cfb25df6d86d46d193ea4d
mingyuchoo <mgch@lgcns.com>
2019-12-11 17:08:24 +0900

    modified

--- react-test-05-user-service/src/components/App.tsx
+++ react-test-05-user-service/src/components/App.tsx
@@ -1,7 +1,23 @@
 import React, { ReactElement } from 'react';
 
+/**
+ * https://www.taniarascia.com/crud-app-in-react-with-hooks/
+ */
+
 export const App: React.FC = (): ReactElement => {
-  return <div>Hello</div>;
+  return (
+    <div className="container">
+      <h1>CRUD App with Hooks</h1>
+      <div className="flex-row">
+        <div className="flex-large">
+          <h2>Add user</h2>
+        </div>
+        <div className="flex-large">
+          <h2>View users</h2>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
