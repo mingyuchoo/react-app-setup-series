31224c0143fd8e55cad36febfcd9703710ebccd7
mingyuchoo <mingyuchoo@gmail.com>
2020-06-17 05:25:05 +0900

    modified

--- typescript/04-added-puppeteer-with-mocha/puppeteer/specs/home-page.spec.ts
+++ typescript/04-added-puppeteer-with-mocha/puppeteer/specs/home-page.spec.ts
@@ -2,24 +2,17 @@ import { should } from 'chai';
 should();
 
 import launchPuppeteer from '../helpers/launchPuppeteer';
-import PageNavigator from '../helpers/PageNavigator';
 
 describe('Main Page test:', () => {
   let browser;
   let page;
-  let pageNav;
-  let mainPage;
 
   before(async () => {
     browser = await launchPuppeteer();
     page = await browser.newPage();
     await page.setViewport({ width: 1280, height: 1024 });
-    pageNav = new PageNavigator(page);
   });
 
-  beforeEach(async () => {
-    mainPage = await pageNav.loadMainPage();
-  });
   it('Default state test', () => {
     '3'.should.equals('3');
   });
