91797d8d20a2283a755d8af705a2df3b50d9b2e1
mingyuchoo <mingyuchoo@gmail.com>
2020-07-02 05:23:44 +0900

    added

---
+++ typescript/14-added-axios-WIP/src/reducers/todos.ts
@@ -0,0 +1,77 @@
+/**
+ * action types
+ */
+const ADD_TODO = 'todos/ADD_TODO' as const;
+const TOGGLE_TODO = 'todos/TOGGLE_TODO' as const;
+const REMOVE_TODO = 'todos/REMOVE_TODO' as const;
+
+/**
+ * action creators
+ */
+
+export const addTodo = (text: string) => ({
+  type: ADD_TODO,
+  payload: text,
+});
+
+export const toggleTodo = (id: number) => ({
+  type: TOGGLE_TODO,
+  payload: id,
+});
+
+export const removeTodo = (id: number) => ({
+  type: REMOVE_TODO,
+  payload: id,
+});
+
+/**
+ * types
+ */
+type TodosAction =
+  | ReturnType<typeof addTodo>
+  | ReturnType<typeof toggleTodo>
+  | ReturnType<typeof removeTodo>;
+
+export type Todo = {
+  id: number;
+  text: string;
+  done: boolean;
+};
+export type TodosState = Todo[];
+
+const initialState: TodosState = [
+  { id: 1, text: 'Pick up my packages', done: true },
+  { id: 2, text: 'Buy Eggs', done: true },
+  { id: 3, text: 'Learn Typescript', done: false },
+];
+
+/**
+ * reducers
+ */
+
+function todos(
+  state: TodosState = initialState,
+  action: TodosAction
+): TodosState {
+  switch (action.type) {
+    case ADD_TODO: {
+      // Unexpected lexical declaration in case block  no-case-declarations
+      const nextId = Math.max(...state.map((todo) => todo.id)) + 1;
+      return state.concat({
+        id: nextId,
+        text: action.payload,
+        done: false,
+      });
+    }
+    case TOGGLE_TODO:
+      return state.map((todo) =>
+        todo.id === action.payload ? { ...todo, done: !todo.done } : todo
+      );
+    case REMOVE_TODO:
+      return state.filter((todo) => todo.id !== action.payload);
+    default:
+      return state;
+  }
+}
+
+export default todos;
