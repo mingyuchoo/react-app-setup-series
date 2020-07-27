8d895b2f4b2a264f1ca50e70f5eff19be926e606
mingyuchoo <mingyuchoo@gmail.com>
2020-07-28 06:57:34 +0900

    make it better

--- basic/09-useReducer/src/App.jsx
+++ basic/09-useReducer/src/App.jsx
@@ -3,11 +3,15 @@ import React from 'react';
 import './App.scss';
 
 import Reservation from './components/Reservation';
+import Example from './components/Example';
 
 function App() {
   return (
     <div className="App">
       <h1> React Reservation App</h1>
+      <hr />
+      <Example />
+      <hr />
       <Reservation />
     </div>
   );
--- basic/09-useReducer/src/components/Reservation.jsx
+++ basic/09-useReducer/src/components/Reservation.jsx
@@ -12,16 +12,16 @@ const userReducer = (state, action) => {
       };
     }
     case 'TOGGLE_TO_BE_ADMIN': {
-      return {...state, isAdmin: !state.isAdmin}
+      return { ...state, isAdmin: !state.isAdmin };
     }
     case 'UPDATE_NICKNAME': {
-      return {...state, nickname: action.nickname}
+      return { ...state, nickname: action.nickname };
     }
     case 'UPDATE_EMAIL': {
-      return {...state, email: action.email}
+      return { ...state, email: action.email };
     }
     default: {
-      throw new Error(`unexpeted action.type: ${action.type}`)
+      throw new Error(`unexpeted action.type: ${action.type}`);
     }
   }
 };
@@ -33,7 +33,7 @@ const Reservation = () => {
     isAdmin: false,
   });
   useEffect(() => {
-    console.log('user is changed')
+    console.log('user is changed');
   }, [user]);
 
   let label = 'user';
@@ -41,21 +41,36 @@ const Reservation = () => {
     label = 'admin';
   }
 
-  const reset           = () => dispatchUser({type: 'RESET'});
-  const toggleToBeAdmin = () => dispatchUser({type: 'TOGGLE_TO_BE_ADMIN'});
-  const updateNickname  = (event) => dispatchUser({type: 'UPDATE_NICKNAME', nickname: event.target.value});
-  const updateEmail     = (event) => dispatchUser({type: 'UPDATE_EMAIL', email: event.target.value})
-
+  const reset = () => dispatchUser({ type: 'RESET' });
+  const toggleToBeAdmin = () => dispatchUser({ type: 'TOGGLE_TO_BE_ADMIN' });
+  const updateNickname = (event) =>
+    dispatchUser({ type: 'UPDATE_NICKNAME', nickname: event.target.value });
+  const updateEmail = (event) =>
+    dispatchUser({ type: 'UPDATE_EMAIL', email: event.target.value });
 
   return (
     <div>
       <label>{label}</label>
       <h1>{user.nickname}</h1>
       <h3>{user.email}</h3>
-      <button className="button" onClick={reset}>RESET</button>
-      <button className="button" onClick={toggleToBeAdmin}>관리자 모드 전환</button>
-      <input className="input" type="text" placeholder="별명" onChange={updateNickname} />
-      <input className="input" type="text" placeholder="이메일" onChange={updateEmail} />
+      <button className="button" onClick={reset}>
+        RESET
+      </button>
+      <button className="button" onClick={toggleToBeAdmin}>
+        관리자 모드 전환
+      </button>
+      <input
+        className="input"
+        type="text"
+        placeholder="별명"
+        onChange={updateNickname}
+      />
+      <input
+        className="input"
+        type="text"
+        placeholder="이메일"
+        onChange={updateEmail}
+      />
     </div>
   );
 };
