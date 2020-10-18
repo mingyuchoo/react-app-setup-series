43200b3a7701b5ab38f61e15f13743085405098a
mingyuchoo <mingyuchoo@gmail.com>
2020-10-19 05:42:02 +0900

    make it better

--- graphql/04-using-cache-with-typescript/src/cache.tsx
+++ graphql/04-using-cache-with-typescript/src/cache.tsx
@@ -1,9 +1,8 @@
 import { InMemoryCache, ReactiveVar, makeVar, gql } from '@apollo/client';
-import { User } from './types/profileTypes';
 
 // reactive variable 선언
-export const nameVar: ReactiveVar<string> = makeVar<string>('');
-export const emailVar: ReactiveVar<string> = makeVar<string>('');
+export const nameVar = makeVar<string>('');
+export const emailVar = makeVar<string>('');
 
 // Field policy for local states
 export const cache = new InMemoryCache({
