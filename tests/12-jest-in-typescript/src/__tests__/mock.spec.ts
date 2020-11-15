c0b81c4f325b8ec37a52fbff0ae6ac79c956cd0d
mingyuchoo <mingyuchoo@gmail.com>
2020-11-16 05:59:06 +0900

    make it better

--- tests/12-jest-in-typescript/src/__tests__/mock.spec.ts
+++ tests/12-jest-in-typescript/src/__tests__/mock.spec.ts
@@ -1,17 +1,35 @@
-describe('mock.spec.ts', () => {
-  test('callback', () => {
-    function forEach(items, callback) {
-      for (let index = 0; index < items.length; index++) {
-        callback(items[index]);
-      }
-    }
+function sum(x, y) {
+  return x + y;
+}
 
+function forEach(items, callback) {
+  for (let index = 0; index < items.length; index++) {
+    callback(items[index]);
+  }
+}
+
+describe('mock.spec.ts', () => {
+  test('2 + 3 return 5', () => {
+    // mocked the function to test
+    const sum = jest.fn((x, y) => x + y);
+
+    // assert
+    expect(sum(2, 3)).toBe(5);
+    expect(sum).toHaveBeenCalled();
+    expect(sum).toHaveBeenCalledTimes(1);
+    expect(sum).toHaveBeenCalledWith(2, 3);
+  });
+
+  test('callback', () => {
+    // mocked callback function using in the function to test
     const mockCallback = jest.fn((x: number) => 42 + x);
 
+    // call the function to test
     forEach([0, 1], mockCallback);
 
     // The mock function is called twice
-    expect(mockCallback.mock.calls.length).toBe(2);
+    expect(mockCallback.mock.calls.length).toBe(2); // because of [0, 1]
+    expect(mockCallback).toHaveBeenCalledTimes(2); // because of [0, 1]
 
     // The first argument of the first call to the function was 0
     expect(mockCallback.mock.calls[0][0]).toBe(0);
