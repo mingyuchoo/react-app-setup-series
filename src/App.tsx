2fc7fa65cdd9131143f33ceeda2c23037638003d
mingyuchoo <mingyuchoo@gmail.com>
2021-01-09 14:12:25 +0900

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,8 @@ import './App.scss';
 
 import React from 'react';
 
-import UserFormConatiner from './containers/UserFormContainer';
-import UserListContainer from './containers/UserListContainer';
-import UserNotiContainer from './containers/UserNotiContainer';
-import UserTitleContainer from './containers/UserTitleContainer';
-
 const App: React.FC = (): React.ReactElement => {
-  return (
-    <div className="App">
-      <UserTitleContainer />
-      <UserNotiContainer />
-      <UserFormConatiner />
-      <UserListContainer />
-    </div>
-  );
+  return <div className="App">Hello</div>;
 };
 
 export default App;
